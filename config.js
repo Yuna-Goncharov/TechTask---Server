ef70da511d86ecf44a5dbefdcf5e2c7c50fc8185
yuna_goncharov <yuna.goncharov@zeek.me>
2018-08-23 14:41:16 +0300

    connection fix

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     host     : 'us-cdbr-iron-east-01.cleardb.net',
     user     : 'bd9924fc0b3651',
-    password:'52026a30',
-    database : 'eroku_1982f1217375ac8'
+    password :'52026a30',
+    database : 'heroku_1982f1217375ac8'
 })
 
 function connection(){
